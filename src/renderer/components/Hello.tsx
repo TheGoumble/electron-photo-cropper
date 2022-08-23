3a21b58ba5e4eb0c02d74fbc27b24e71a9184d1d
Javier Vargas <3231javiervargas@gmail.com>
2022-08-23 10:00:17 -0400

    added Photo component

---
+++ src/renderer/components/Hello.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+
+export default function Hello() {
+  return (
+    <div>
+      <h1>My Photo Cropper</h1>
+      <div className="Hello">
+        <Link to="/Photo">
+          <button type="button">
+            <span role="img" aria-label="books">
+              📸
+            </span>
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+}
